ec347926269dbdba61e66540b2b2ae279e491e5a
captainzonks <zonks.zonks@gmail.com>
2020-03-09 15:21:12 -0600

    clean up

--- rpgProject/main.cpp
+++ rpgProject/main.cpp
@@ -8,13 +8,5 @@ int main()
     Game game;
     game.GameLoop();
 
-    
-    //cout << "Inventory Capacity: " << player->myInventory.getCapacity() << endl;
-    //cout << player->GetName() << "'s Money" << endl;
-    //player->myCurrency.displayMoney();
-
-    //player->myInventory.addToInventory(narrator.MakeAnItem(Narrator::ITEM_TYPES::SWORD));
-    //player->myInventory.displayInventory();
-
     return 0;
 }
