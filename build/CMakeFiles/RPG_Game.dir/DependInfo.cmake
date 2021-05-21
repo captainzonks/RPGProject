22cd2bf8689f343b1402e54fece7f75fbb682774
Matthew Barham <zonks.zonks@gmail.com>
2021-05-21 15:53:06 -0600

    Added CMake required files and compile commands json file for autocompletion

---
+++ build/CMakeFiles/RPG_Game.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Documents/cpp/RPGProject/source/attributes_component.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/attributes_component.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/character_builder.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/character_builder.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/character_creator_state.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/character_creator_state.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/class_features.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/class_features.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/class_functions.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/class_functions.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/combat_state.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/combat_state.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/component.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/component.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/currency_component.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/currency_component.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/entity.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/entity.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/entity_manager.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/entity_manager.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/game.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/game.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/game_state.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/game_state.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/inventory_component.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/inventory_component.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/item.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/item.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/item_list.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/item_list.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/job_component.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/job_component.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/job_functions.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/job_functions.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/location.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/location.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/main.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/main.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/map_menu_state.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/map_menu_state.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/menu_state.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/menu_state.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/npc_info_menu.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/npc_info_menu.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/player_info_menu.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/player_info_menu.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/shop_menu_state.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/shop_menu_state.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/spacial_component.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/spacial_component.cpp.o"
+  "/home/pi/Documents/cpp/RPGProject/source/world.cpp" "/home/pi/Documents/cpp/RPGProject/build/CMakeFiles/RPG_Game.dir/world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pi/Documents/cpp/RPGProject/source"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RPG_Game.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPG_Game.dir/attributes_component.cpp.o"
+  "CMakeFiles/RPG_Game.dir/character_builder.cpp.o"
+  "CMakeFiles/RPG_Game.dir/character_creator_state.cpp.o"
+  "CMakeFiles/RPG_Game.dir/class_features.cpp.o"
+  "CMakeFiles/RPG_Game.dir/class_functions.cpp.o"
+  "CMakeFiles/RPG_Game.dir/combat_state.cpp.o"
+  "CMakeFiles/RPG_Game.dir/component.cpp.o"
+  "CMakeFiles/RPG_Game.dir/currency_component.cpp.o"
+  "CMakeFiles/RPG_Game.dir/entity.cpp.o"
+  "CMakeFiles/RPG_Game.dir/entity_manager.cpp.o"
+  "CMakeFiles/RPG_Game.dir/game.cpp.o"
+  "CMakeFiles/RPG_Game.dir/game_state.cpp.o"
+  "CMakeFiles/RPG_Game.dir/inventory_component.cpp.o"
+  "CMakeFiles/RPG_Game.dir/item.cpp.o"
+  "CMakeFiles/RPG_Game.dir/item_list.cpp.o"
+  "CMakeFiles/RPG_Game.dir/job_component.cpp.o"
+  "CMakeFiles/RPG_Game.dir/job_functions.cpp.o"
+  "CMakeFiles/RPG_Game.dir/location.cpp.o"
+  "CMakeFiles/RPG_Game.dir/main.cpp.o"
+  "CMakeFiles/RPG_Game.dir/map_menu_state.cpp.o"
+  "CMakeFiles/RPG_Game.dir/menu_state.cpp.o"
+  "CMakeFiles/RPG_Game.dir/npc_info_menu.cpp.o"
+  "CMakeFiles/RPG_Game.dir/player_info_menu.cpp.o"
+  "CMakeFiles/RPG_Game.dir/shop_menu_state.cpp.o"
+  "CMakeFiles/RPG_Game.dir/spacial_component.cpp.o"
+  "CMakeFiles/RPG_Game.dir/world.cpp.o"
+  "RPG_Game"
+  "RPG_Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPG_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
